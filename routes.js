5a3e4da77ef6c0238c825b1475731c1182247d9f
Tomer Amir <tomer.amir@vonage.com>
2018-11-29 16:41:00 +0000

    Added user route

--- routes.js
+++ routes.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const { sendMessage } = require('./nexmo');
+const users = require('./routes/users');
 
 router.get('/call-for-help', (req, res, next) => {
     sendMessage();
     res.send({
         msg: 'Getting Help'
     });
-})
+});
+
+router.use('/user', users);
 
 module.exports = router;
