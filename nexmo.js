52fc0e0699a101ef9d42db2e28d6e81d377b368e
Alexandre Perles <aperles@VONFVFXK06BHV2H.local>
2018-11-29 15:26:09 +0000

    Message api

---
+++ nexmo.js
@@ -0,0 +1,27 @@
+const Nexmo = require('nexmo')
+
+
+
+module.exports = {
+  sendMessage: () => {
+
+    const nexmo = new Nexmo({
+      apiKey: "195db385",
+      apiSecret: "0ESc2zHL9YQiT5ng",
+      applicationId: "ff35c24a-6155-4c1b-a10f-885512e3f286",
+      privateKey: "private.key"
+    });
+    
+    nexmo.channel.send(
+      { "type": "sms", "number": "447521175057" },
+      { "type": "sms", "number": "447521175057" },
+      {
+        "content": {
+          "type": "text",
+          "text": "This is an SMS sent from the Messages API"
+        }
+      },
+      (err, data) => { console.log(data.message_uuid); }
+    );
+  }
+};
--- routes.js
+++ routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const { sendMessage } = require('./nexmo');
 
 router.get('/call-for-help', (req, res, next) => {
+    sendMessage();
     res.send({
         msg: 'Getting Help'
     });
