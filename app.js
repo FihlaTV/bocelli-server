b43d1203853a1f0ee33cfbccb2441e79b000ea02
Tomer Amir <tomer.amir@vonage.com>
2018-11-29 14:59:29 +0000

    First Commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const app = require('express')(http);
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.post('/tomer', (req, res, next) => {
+    console.log(req.body);
+    res.send('hi there');
+});
+
+app.use((req, res, next) => {
+    res.status(404);
+    res.send('Not Found');
+})
+
+app.listen(3000, () => {
+    console.log('server is running on port 3000');
+});
